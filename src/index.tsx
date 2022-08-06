f1244c1f33a4831e65c5076b991709879314738a
ikura1 <ikura.lab@gmail.com>
2022-08-06 20:51:35 +0900

    feat: onClickでstateの変更と表示 - typeをstringに修正

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ type SquareProps = {
 };
 
 type SquareState = {
-  value: number | null;
+  value: string | null;
 };
 
 class Square extends React.Component<SquareProps, SquareState> {
@@ -20,8 +20,8 @@ class Square extends React.Component<SquareProps, SquareState> {
 
   render() {
     return (
-      <button className="square" onClick={() => console.log("click")}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
       </button>
     );
   }
