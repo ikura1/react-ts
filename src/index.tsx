dde1fb468ea029d1e7877930feb704164947b39b
ikura1 <ikura.lab@gmail.com>
2022-08-07 02:30:48 +0900

    feat: 手番処理を追加

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ type BoardProps = {};
 
 type BoardState = {
   squares: Array<string | null>;
+  xIsNext: boolean;
 };
 
 type SquareProps = {
@@ -26,13 +27,14 @@ class Board extends React.Component<BoardProps, BoardState> {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i: number) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i: number) {
@@ -45,7 +47,7 @@ class Board extends React.Component<BoardProps, BoardState> {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
