d448e8d395d001cfe7d654007eafc804f25d58fd
ikura1 <ikura.lab@gmail.com>
2022-08-07 14:43:58 +0900

    feat: 過去の盤面に戻れる機能を追加

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ type GameProps = {};
 
 type GameState = {
   history: Array<{ squares: Squares }>;
+  stepNumber: number;
   xIsNext: boolean;
 };
 
@@ -71,12 +72,13 @@ class Game extends React.Component<GameProps, GameState> {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i: number) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -85,14 +87,31 @@ class Game extends React.Component<GameProps, GameState> {
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((_, move) => {
+      const desc = move ? `Go to move #${move}` : `Go to game start`;
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status: string;
     if (winner) {
@@ -110,7 +129,7 @@ class Game extends React.Component<GameProps, GameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
